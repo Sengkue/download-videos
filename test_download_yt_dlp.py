a3bdbd0935a8d6697ff30ae0579aca1bf42ecb6a
sengkue <skvgithub@gmail.com>
2024-10-09 19:32:41 +0700

    commit Wed 10/09/2024 19:32:41.15

---
+++ test_download_yt_dlp.py
@@ -0,0 +1,14 @@
+import yt_dlp
+
+url = 'https://www.youtube.com/watch?v=gRCnrjs0Ki0'  # Use the full URL
+
+ydl_opts = {
+    'outtmpl': 'downloads/%(title)s.%(ext)s',  # Save to downloads folder
+}
+
+try:
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    print("Download completed!")
+except Exception as e:
+    print(f"Error: {e}")
