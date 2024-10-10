6c5abcdf5acb5b5813576c41d7d4c5bc85bace1f
sengvang.blo <skvgithub@gmail.com>
2024-10-10 09:18:39 +0700

    commit 10/10/2024  9:18:39.17

--- static/script.js
+++ static/script.js
@@ -2,6 +2,7 @@
 function pasteLink() {
     navigator.clipboard.readText().then(function(text) {
         document.getElementById('video-url').value = text;
+        toggleClearIcon(); // Show clear icon after pasting
     }).catch(function(err) {
         console.error('Failed to read clipboard: ', err);
     });
@@ -9,4 +10,17 @@ function pasteLink() {
 
 function clearInput() {
     document.getElementById('video-url').value = ''; // Clear the input field
+    toggleClearIcon(); // Hide clear icon after clearing
+}
+
+function toggleClearIcon() {
+    const inputField = document.getElementById('video-url');
+    const clearIcon = document.querySelector('.clear-icon');
+
+    // Show clear icon if the input field has value
+    if (inputField.value) {
+        clearIcon.style.display = 'block';
+    } else {
+        clearIcon.style.display = 'none';
+    }
 }
